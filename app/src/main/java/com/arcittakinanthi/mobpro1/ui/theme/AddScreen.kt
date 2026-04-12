70487793e20984d44f228b9f7b9f8247c1812138
Arcitta Kinanthi <cittacittul@gmail.com>
2026-04-13 03:14:30 +0700

    fix: final home screen with working implicit intent

--- app/src/main/java/com/arcittakinanthi/mobpro1/ui/theme/AddScreen.kt
+++ app/src/main/java/com/arcittakinanthi/mobpro1/ui/theme/AddScreen.kt
@@ -1,11 +1,26 @@
 package com.arcittakinanthi.mobpro1.ui.theme
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -23,7 +38,7 @@ fun AddScreen(onBackClick: () -> Unit) {
                 title = { Text("Tambah Transaksi") },
                 navigationIcon = {
                     IconButton(onClick = onBackClick) {
-                        Icon(Icons.Default.ArrowBack, contentDescription = "Kembali")
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Kembali")
                     }
                 }
             )
--- app/src/main/java/com/arcittakinanthi/mobpro1/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/arcittakinanthi/mobpro1/ui/theme/HomeScreen.kt
@@ -1,12 +1,25 @@
 package com.arcittakinanthi.mobpro1.ui.theme
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -24,7 +37,7 @@ fun HomeScreen(
                 title = { Text("Manajer Uangku") },
                 actions = {
                     IconButton(onClick = { }) {
-                        Icon(Icons.Default.Info, contentDescription = "Tentang Aplikasi")
+                        Icon(imageVector = Icons.Default.Info, contentDescription = "Tentang Aplikasi")
                     }
                 }
             )
