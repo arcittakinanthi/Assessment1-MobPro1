013d5609c6c8e49f2ec26b8e937b69a656f6e963
Arcitta Kinanthi <cittacittul@gmail.com>
2026-04-13 03:29:25 +0700

    fix: finalllll commit

--- app/src/main/java/com/arcittakinanthi/mobpro1/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/arcittakinanthi/mobpro1/ui/theme/HomeScreen.kt
@@ -1,17 +1,22 @@
 package com.arcittakinanthi.mobpro1.ui.theme
 
+import android.content.Intent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -21,8 +26,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
+import com.arcittakinanthi.mobpro1.R
 import com.arcittakinanthi.mobpro1.model.Transaksi
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,20 +41,28 @@ fun HomeScreen(
     listTransaksi: List<Transaksi>,
     onAddClick: () -> Unit
 ) {
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text("Manajer Uangku") },
                 actions = {
+                    IconButton(onClick = {
+                        val intent = Intent(Intent.ACTION_VIEW, "https://google.com".toUri())
+                        context.startActivity(intent)
+                    }) {
+                        Icon(imageVector = Icons.Default.Share, contentDescription = "Buka Browser")
+                    }
                     IconButton(onClick = { }) {
-                        Icon(imageVector = Icons.Default.Info, contentDescription = "Tentang Aplikasi")
+                        Icon(imageVector = Icons.Default.Info, contentDescription = "Info")
                     }
                 }
             )
         },
         floatingActionButton = {
             FloatingActionButton(onClick = onAddClick) {
-                Icon(Icons.Default.Add, contentDescription = "Tambah Transaksi")
+                Icon(Icons.Default.Add, contentDescription = "Tambah")
             }
         }
     ) { innerPadding ->
@@ -53,14 +71,28 @@ fun HomeScreen(
                 .padding(innerPadding)
                 .fillMaxSize(),
             contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            item {
+                Image(
+                    painter = painterResource(id = R.drawable.apkKu),
+                    contentDescription = "Logo",
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(bottom = 20.dp)
+                )
+            }
+
             items(listTransaksi) { transaksi ->
-                Card(modifier = Modifier.fillMaxWidth()) {
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    elevation = CardDefaults.cardElevation(4.dp)
+                ) {
                     Column(modifier = Modifier.padding(16.dp)) {
                         Text(text = transaksi.nama, style = MaterialTheme.typography.titleMedium)
-                        Text(text = "Rp ${transaksi.nominal}", style = MaterialTheme.typography.bodyMedium)
-                        Text(text = "Kategori: ${transaksi.kategori}", style = MaterialTheme.typography.labelSmall)
+                        Text(text = "Rp ${transaksi.nominal}", color = MaterialTheme.colorScheme.primary)
+                        Text(text = "Kategori: ${transaksi.kategori}", style = MaterialTheme.typography.bodySmall)
                     }
                 }
             }
