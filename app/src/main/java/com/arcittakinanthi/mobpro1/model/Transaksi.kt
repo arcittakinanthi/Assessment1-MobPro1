ea286898da11c2c2d587cedb446733f52666a520
Arcitta Kinanthi <cittacittul@gmail.com>
2026-05-13 00:00:56 +0700

    feat: convert Transaksi class to Room Entity

--- app/src/main/java/com/arcittakinanthi/mobpro1/model/Transaksi.kt
+++ app/src/main/java/com/arcittakinanthi/mobpro1/model/Transaksi.kt
@@ -1,6 +1,12 @@
 package com.arcittakinanthi.mobpro1.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "transaksi_table")
 data class Transaksi(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val nama: String,
     val nominal: String,
     val kategori: String
